8f92cc566fb61d9564e5bf68b9270bab625dc5b0
ikkrit <johan.car@protonmail.com>
2022-11-02 22:30:21 +0100

    mise en forme

--- Views/home/home_index.html.php
+++ Views/home/home_index.html.php
@@ -1,9 +1,13 @@
-<?php require_once ROOT.'/Views/home/components/_categories.html.php';?>
+<div class="container">
 
-<?php require_once ROOT.'/Views/home/components/_world.html.php';?>
+    <?php require_once ROOT.'/Views/home/components/_categories.html.php';?>
 
-<?php require_once ROOT.'/Views/home/components/_characters.html.php';?>
+    <?php require_once ROOT.'/Views/home/components/_world.html.php';?>
 
-<?php require_once ROOT.'/Views/home/components/_game.html.php';?>
+    <?php require_once ROOT.'/Views/home/components/_characters.html.php';?>
 
-<?php require_once ROOT.'/Views/home/components/_hoyoverse.html.php';?>
+    <?php require_once ROOT.'/Views/home/components/_game.html.php';?>
+
+    <?php require_once ROOT.'/Views/home/components/_hoyoverse.html.php';?>
+
+</div>
